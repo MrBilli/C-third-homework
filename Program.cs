c87cca7ab1874d6ac978dfceafb04b46b815bf28
Alexey <leha.basanov123@gmail.com>
2023-03-15 23:59:28 +0300

    C# Homework 3 last changes

--- Program.cs
+++ Program.cs
@@ -33,31 +33,26 @@ double distance( int x1, int y1, int z1, int x2, int y2, int z2)
     return Math.Round(Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2) + Math.Pow((z1 - z2), 2 )), 2);
 }
 
-Console.WriteLine("Введите x1:");
+int input(string dot)
+{
+    Console.WriteLine($"Введите точку {dot}:");
 
-int x1 = Convert.ToInt32(Console.ReadLine());
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-Console.WriteLine("Введите y1:");
+int x1 = input("x1");
 
-int y1 = Convert.ToInt32(Console.ReadLine());
+int y1 = input("y1");
 
-Console.WriteLine("Введите z1:");
+int z1 = input("z1");;
 
-int z1 = Convert.ToInt32(Console.ReadLine());
+int x2 = input("x2");;
 
-Console.WriteLine("Введите x2");
+int y2 = input("y2");;
 
-int x2 = Convert.ToInt32(Console.ReadLine());
+int z2 = input("z2");;
 
-Console.WriteLine("Введите y2");
-
-int y2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите z2");
-
-int z2 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine(distance(x1, y1, z1, x2, y2, z2));
+Console.WriteLine($"Расстояние между точками равно {distance(x1, y1, z1, x2, y2, z2)}");
 
 */
 
